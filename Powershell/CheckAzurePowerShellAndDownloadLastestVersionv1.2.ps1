c663366d057987fe4eecd6f67d8d716b18aa91f4
Dave Chianese <david.chianese@freudenberg-it.com>
2016-05-11 07:19:26 -0400

    Added folders back into repo for Powershell and scripts

---
+++ Powershell/CheckAzurePowerShellAndDownloadLastestVersionv1.2.ps1
@@ -0,0 +1,89 @@
+﻿# Author Peter Selch Dahl from ProActive A/S
+# Revised by Anders Huusom from ProActive A/S
+# Version 1.2
+
+#import Azure Powershell
+
+If (!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+    Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
+    Break
+}
+
+
+ Import-Module "C:\Program Files (x86)\Microsoft SDKs\Azure\PowerShell\ServiceManagement\azure\azure.psd1"
+
+ function Install-MSIFile {
+
+[CmdletBinding()]
+ Param(
+  [parameter(mandatory=$true,ValueFromPipeline=$true,ValueFromPipelinebyPropertyName=$true)]
+        [ValidateNotNullorEmpty()]
+        [string]$msiFile,
+
+        [parameter()]
+        [ValidateNotNullorEmpty()]
+        [string]$targetDir
+ )
+if (!(Test-Path $msiFile)){
+    throw "Path to the MSI File $($msiFile) is invalid. Please supply a valid MSI file"
+}
+$arguments = @(
+    "/i"
+    "`"$msiFile`""
+    "/qn"
+    "/norestart"
+)
+if ($targetDir){
+    if (!(Test-Path $targetDir)){
+        throw "Path to the Installation Directory $($targetDir) is invalid. Please supply a valid installation directory"
+    }
+    $arguments += "INSTALLDIR=`"$targetDir`""
+}
+Write-Verbose "Installing $msiFile....."
+$process = Start-Process -FilePath msiexec.exe -ArgumentList $arguments -Wait -PassThru
+if ($process.ExitCode -eq 0){
+    Write-Verbose "$msiFile has been successfully installed"
+}
+else {
+    Write-Verbose "installer exit code  $($process.ExitCode) for file  $($msifile)"
+}
+}
+
+
+#write installed Azure PoSh module version
+ Write-Host ""
+ Write-host "Checking Azure Powershell Version.."
+ Write-Host ""
+ Write-Host "Your Installed Azure Powershell Version is: " -NoNewline; Write-Host -ForegroundColor "Yellow" (Get-Module -Name azure).version.tostring()
+ 
+ 
+ $CurrentInstalledAzurePowershellVersion = (Get-Module -Name azure).version.tostring()
+ 
+#code block to check online version
+ $web=(Invoke-Webrequest -Uri "http://github.com/Azure/azure-powershell/releases" -MaximumRedirection 1).links | where {$_.outertext -like "Windows Standalone"}
+ 
+ $NewAzurePowershellVersion = ($web[0].href.Split("/")[($web[0].href.split("/")).length-1].trim("azure-powershell.").trim(".msi"))
+ 
+ Write-Host "Lastest version online is: " -NoNewline;Write-Host -ForegroundColor "Yellow" ($web[0].href.Split("/")[($web[0].href.split("/")).length-1].trim("azure-powershell.").trim(".msi"))
+ Write-Host ""
+
+ if ($NewAzurePowershellVersion -ne $CurrentInstalledAzurePowershellVersion) 
+ {
+
+
+  Write-Host "MSI Direct Download Link: " -NoNewline;Write-Host -ForegroundColor Yellow $web[0].href
+  Write-Host "Reference: https://github.com/Azure/azure-powershell/releases"
+  Write-Host ""
+
+  $source = $web[0].href
+  $destination =  $Env:temp + "\" + ($web[0].href.Split("/")[($web[0].href.split("/")).length-1])
+  
+  Write-Host "Downloading MSI File....."
+  Write-Host ""
+
+  Invoke-WebRequest $source -OutFile $destination
+
+  $destination| Install-MSIFile -Verbose
+
+ }
---
+++ Powershell/Get-RMUsageAggregates.ps1
@@ -0,0 +1,77 @@
+﻿function Get-RMUsageAggregates {
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory=$true,ValueFromPipeline=$true)]
+        [DateTime]$reportedStartTime,
+
+        [parameter(Mandatory=$true,ValueFromPipeline=$true)]
+        [DateTime]$reportedEndTime,
+        
+        [parameter(Mandatory=$true,ValueFromPipeline=$true)]
+        [string]$resourceGroup,
+
+        [parameter(Mandatory=$true,ValueFromPipeline=$true)]
+        [string]$subscriptionId,
+
+        [parameter(Mandatory=$true,ValueFromPipeline=$true)]
+        [string]$granularity
+        
+    )
+    PROCESS {
+
+        
+        Select-AzureRMSubscription -SubscriptionId $subscriptionId
+
+        # Set usage parameters
+
+        $showDetails = $true
+
+        # Export Usage to CSV
+
+        $appendFile = $false
+
+        $continuationToken = ""
+
+        Add-Type -AssemblyName System.Web
+
+        Do { 
+
+            $usageData = Get-UsageAggregates `
+                -ReportedStartTime $reportedStartTime `
+                -ReportedEndTime $reportedEndTime `
+                -AggregationGranularity $granularity `
+                -ShowDetails:$showDetails `
+                -ContinuationToken $continuationToken
+
+            Write-Output [-InputObject $usageData.UsageAggregations.Properties | 
+                Where-Object {$_.InstanceData -like "*resourceGroups/$resourceGroup*"} |
+                Select-Object `
+                    UsageStartTime, `
+                    UsageEndTime, `
+                    @{n='SubscriptionId';e={$subscriptionId}}, `
+                    MeterCategory, `
+                    MeterId, `
+                    MeterName, `
+                    MeterSubCategory, `
+                    MeterRegion, `
+                    Unit, `
+                    Quantity, `
+                    @{n='Project';e={$_.InfoFields.Project}}, `
+                    InstanceData
+
+            if ($usageData.NextLink) {
+
+                $continuationToken = `
+                    [System.Web.HttpUtility]::UrlDecode($usageData.NextLink.Split("=")[-1])
+
+            } else {
+
+                $continuationToken = ""
+
+            }
+
+            $appendFile = $true
+
+        } until (!$continuationToken)
+    }
+}
